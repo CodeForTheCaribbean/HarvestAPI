426e2c0d2aeae69293761f7f9826ea9eecb8e79a
Varun Baker <varun@developdigitally.com>
2013-11-06 16:21:11 -0500

    added tutorial one: Serialization http://django-rest-framework.org/tutorial/1-serialization.html

---
+++ tutorial/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('snippets.views',
+    url(r'^snippets/$', 'snippet_list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
+)
