c06066430cbe101ca8056420a10d4ab49be423ca
Varun Baker <varun@developdigitally.com>
2013-11-11 16:17:52 -0500

    added error handling and request/response code

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,18 +1,15 @@
 from django.conf.urls import patterns, include, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^', include('snippets.urls')),
-    # Examples:
-    # url(r'^$', 'tutorial.views.home', name='home'),
-    # url(r'^tutorial/', include('tutorial.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('snippets.views',
+    url(r'^snippets/$', 'snippet_list'),
+    url(r'^snippets/(?P<pk>[0-9]+)$', 'snippet_detail'),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+
