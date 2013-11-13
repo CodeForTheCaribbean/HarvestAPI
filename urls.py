3d656d71e5373f4b675de7aa6e9cfedd8a68ea0b
Varun Baker <varun@developdigitally.com>
2013-11-13 12:58:47 -0500

    added class based views - tutorial 3

--- urls.py
+++ urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
 
-urlpatterns = patterns('snippets.views',
-    url(r'^snippets/$', 'snippet_list'),
-    url(r'^snippets/(?P<pk>[0-9]+)$', 'snippet_detail'),
+urlpatterns = patterns('',
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
