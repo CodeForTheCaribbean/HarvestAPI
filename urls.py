2537a919b779db33025f026a8d315a90ee352f0a
Varun Baker <varun@developdigitally.com>
2013-11-15 12:37:11 -0500

    tut 5 creates hyperlinked endpoints for the highlighted snippets and added pagination

--- urls.py
+++ urls.py
@@ -3,13 +3,28 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 from django.conf.urls import include
 
-urlpatterns = patterns('',
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+# API endpoints
+urlpatterns = format_suffix_patterns(patterns('snippets.views',
+    url(r'^$', 'api_root'),
+    url(r'^snippets/$',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail')
+))
+
+# Login and logout views for the browsable API
+urlpatterns += patterns('',    
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 )
-
-urlpatterns = format_suffix_patterns(urlpatterns)
