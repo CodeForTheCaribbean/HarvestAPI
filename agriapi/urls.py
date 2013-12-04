f6312edaff99a2b6c6b7896ba025cb153b8c3f8f
Varun Baker <varun@developdigitally.com>
2013-12-04 16:36:35 -0500

    added farmer resource

---
+++ agriapi/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from rest_framework.urlpatterns import format_suffix_patterns
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('farmers.views',
+    url(r'^farmers/$', 'farmer_list'),
+    url(r'^farmers/(?P<pk>[0-9]+)$', 'farmer_detail'),
+)
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+
---
+++ farmers/serializers.py
@@ -0,0 +1,20 @@
+from django.forms import widgets
+from rest_framework import serializers
+from farmers.models import Farmer, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+class FarmerSerializer(serializers.HyperlinkedModelSerializer):
+#    owner = serializers.Field(source='owner.username')
+#    highlight = serializers.HyperlinkedIdentityField(view_name='farmer-highlight', format='html')
+
+    class Meta:
+        model = Farmer 
+        fields = ('farmer_idx','farmer_id','first_name','last_name','alias','res_address', 'res_parish','tel_number','cell_number','verified_status','dob','agri_activity')#'owner')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    farmers = serializers.HyperlinkedRelatedField(many=True, view_name='farmer-detail')
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'farmers')
