52c15d72262085536373d8703781ea27c3597fd7
Varun Baker <varun@developdigitally.com>
2013-12-05 12:17:59 -0500

    added url and owner fields

--- agriapi/urls.py
+++ agriapi/urls.py
@@ -7,7 +7,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('farmers.views',
     url(r'^farmers/$', 'farmer_list'),
-    url(r'^farmers/(?P<pk>[0-9]+)$', 'farmer_detail'),
+    url(r'^farmers/(?P<pk>[0-9]+)$', 'farmer_id'),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
