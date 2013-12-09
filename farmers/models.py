4efd9d2450f6145f3e0aa92b7e7e5d413eaa7cc5
Rory Walker <redrory@gmail.com>
2013-12-09 11:02:58 -0500

    Added PK to RB, RB Search now works

--- farmers/models.py
+++ farmers/models.py
@@ -22,8 +22,8 @@ class Farmer(models.Model):
     tel_number = models.CharField(max_length=20, blank=True, default='')
     cell_number = models.CharField(max_length=20, blank=True, default='')
     verified_status = models.CharField(max_length=3, blank=True, default='')
-    dob = models.DateTimeField() 
-#    reg_date = models.DateTimeField() 
+    dob = models.DateTimeField()
+#    reg_date = models.DateTimeField()
     agri_activity = models.CharField(max_length=150, blank=True, default='')
     owner = models.ForeignKey('auth.User', related_name='farmers')
 #    created = models.DateTimeField(auto_now_add=True)
@@ -46,7 +46,7 @@ class Farmer(models.Model):
 
 class Receipt(models.Model):
     farmer_idx = models.IntegerField(max_length=6)
-    receipt_no =  models.CharField(max_length=12, blank=False, default='')
+    receipt_no =  models.CharField(max_length=12, blank=False, default='', primary_key=True)
     rec_range1 = models.CharField(max_length=100, blank=True, default='')
     rec_range2 = models.CharField(max_length=100, blank=True, default='')
     investigation_status = models.CharField(max_length=100, blank=True, default='')
