e1ea5b04ff5dffc5c1aac3e64a32787cebea4a0b
Varun Baker <varun@developdigitally.com>
2013-12-10 10:36:08 -0500

    linked farmers to receipts

--- models.tmp.py
+++ models.tmp.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Farmer(models.Model):
 
-    farmer_idx = models.IntegerField(max_length=6)
-    farmer_id =  models.CharField(max_length=100, blank=False, default='', primary_key=True)
+    farmer_idx = models.BigIntegerField(max_length=6)
+    farmer_id =  models.CharField(max_length=10, blank=False, default='', primary_key=True)
     first_name = models.CharField(max_length=100, null=True, default='')
     last_name = models.CharField(max_length=100, null=True, default='')
     alias = models.CharField(max_length=100, null=True, default='')
@@ -15,23 +15,18 @@ class Farmer(models.Model):
     dob = models.DateTimeField()
 #    reg_date = models.DateTimeField()
     agri_activity = models.CharField(max_length=150, null=True, default='')
-    owner = models.ForeignKey('auth.User', related_name='farmers', default=1)
+    owner = models.ForeignKey('auth.User', related_name='farmers')
 #    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('dob',)
 
 class Receipt(models.Model):
-    farmer_idx = models.CharField(max_length=100)
-    receipt_no =  models.CharField(max_length=100, null=False, default='')
+    farmer_id = models.ForeignKey(Farmer, related_name='receipts')
+    farmer_idx = models.BigIntegerField(max_length=6)
+    receipt_no =  models.CharField(max_length=12, blank=False, default='', primary_key=True)
     rec_range1 = models.CharField(max_length=100, null=True, default='')
     rec_range2 = models.CharField(max_length=100, null=True, default='')
     investigation_status = models.CharField(max_length=100, null=True, default='')
     remarks = models.CharField(max_length=200, null=True, default='')
-    farmer = models.ForeignKey(Farmer, related_name='receipts')
-
-    class Meta:
-        unique_together = ('farmer', 'farmer_idx')
-
-    def __unicode__(self):
-        return 'rec_no: %s' % (self.receipt_no)
+#    farmer = models.ForeignKey(Farmer, related_name='farmer_id')
