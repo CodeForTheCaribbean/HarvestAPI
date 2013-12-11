2190522fbd11dd0381c5e9e6303802f8c93474c2
Varun Baker <varun@developdigitally.com>
2013-12-10 19:57:55 -0500

    added farmer info to receipt endpoints with farmer_id as pk

--- farmers/migrations/0004_initial.py
+++ farmers/migrations/0004_initial.py
@@ -38,13 +38,13 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal(u'farmers', ['Receipt'])
 
-        # Adding unique constraint on 'Receipt', fields ['receipt_no', 'farmer_idx']
-        db.create_unique(u'farmers_receipt', ['receipt_no', 'farmer_idx_id'])
+        # Adding unique constraint on 'Receipt', fields ['rec_range1', 'rec_range2']
+        db.create_unique(u'farmers_receipt', ['rec_range1', 'rec_range2'])
 
 
     def backwards(self, orm):
-        # Removing unique constraint on 'Receipt', fields ['receipt_no', 'farmer_idx']
-        db.delete_unique(u'farmers_receipt', ['receipt_no', 'farmer_idx_id'])
+        # Removing unique constraint on 'Receipt', fields ['rec_range1', 'rec_range2']
+        db.delete_unique(u'farmers_receipt', ['rec_range1', 'rec_range2'])
 
         # Deleting model 'Farmer'
         db.delete_table(u'farmers_farmer')
@@ -107,7 +107,7 @@ class Migration(SchemaMigration):
             'verified_status': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '3', 'null': 'True'})
         },
         u'farmers.receipt': {
-            'Meta': {'unique_together': "(('receipt_no', 'farmer_idx'),)", 'object_name': 'Receipt'},
+            'Meta': {'unique_together': "(('rec_range1', 'rec_range2'),)", 'object_name': 'Receipt'},
             'farmer_idx': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'receipts'", 'to': u"orm['farmers.Farmer']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'investigation_status': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'null': 'True'}),
