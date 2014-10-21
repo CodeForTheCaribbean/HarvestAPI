dd3061c929246b556ae77da29d07f0cd5aec9dc0
varunity <varun.baker@gmail.com>
2014-10-21 15:47:08 -0500

    edited travis file and DB settings

--- agriapi/settings.py
+++ agriapi/settings.py
@@ -165,7 +165,7 @@ LOGGING = {
 #added for Heroku
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres@localhost/harvest_api')}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
