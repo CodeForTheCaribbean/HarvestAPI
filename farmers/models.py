6a0b2fc05c034abe0b1a62e7e8d3d478ab0bf51a
Matthew Budram <matjames007@gmail.com>
2015-01-28 15:37:29 -0500

    I removed some depracated methods from the models file.  This still requires a bit of testing but from what I see everything still works.
    Note that django-registration needs to be removed from your library
    for this to work.  Try using pip uninstall django-registration.
    django registration does not have support for 1.7 and now the
    django-registration-redux is used.  It has the same functionality
    as previous versions.  django-registration-redux is a fork of the
    django-registration because the app is no longer maintained by the
    owner.
    
    Please do a syncdb or migrate before launching!
    
    Matthew Budram

--- farmers/models.py
+++ farmers/models.py
@@ -1,7 +1,6 @@
 import datetime, hashlib, random, re
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -9,10 +8,10 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 
-@receiver(post_save, sender=get_user_model())
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+# @receiver(post_save, sender=get_user_model())
+# def create_auth_token(sender, instance=None, created=False, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
 
 
 class Farmer(models.Model):
