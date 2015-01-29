1d3f4dd731a4990ff0c02600fb6e60b85018b971
Matthew Budram <matjames007@gmail.com>
2015-01-29 16:15:59 -0500

    I am removing the rest framework's declaration for authtoken.  This seems to be problematic for @varun and @anna-marie but until we figure out the purpose of authtoken it will be commented.
    Matthew Budram

--- harvestapi/settings.py
+++ harvestapi/settings.py
@@ -99,7 +99,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
     'farmers',
     'rest_framework_swagger',
     'registration',
